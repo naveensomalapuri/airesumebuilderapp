cb11eb2a675150b38fb1d43ea9d1dcefcfbcb378
naveensomalapuri <99134100+naveensomalapuri@users.noreply.github.com>
2024-08-31 20:24:46 +0530

    commented staticfiles function

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
 from routes.resume_routes import router as resume_router
-from fastapi.staticfiles import StaticFiles
+# from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
 # Mount static files
-app.mount("/static", StaticFiles(directory="static"), name="static")
+# app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Set up templates
 templates = Jinja2Templates(directory="templates")
